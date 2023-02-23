74a0c065ca993fe6a793850c14fe0819e039dc31
Anthony Chambers <ajfchambers@gmail.com>
2023-02-23 14:52:31 +0000

    Emoji strings refactored

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 const assertEqual = function(actual, expected) {
   //console.assert(actual === expected);
-  const passed = String.fromCodePoint(0x2705);
-  const failed = String.fromCodePoint(0x274C);
+  const passed = String.fromCodePoint(0x2705, 0x2705, 0x2705);
+  const failed = String.fromCodePoint(0x274C, 0x274C, 0x274C);
   if (actual === expected) {
-    return console.log(`${passed}${passed}${passed} Assertion Passed: ${actual} === ${expected}`);
+    return console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
   }
-  return console.log(`${failed}${failed}${failed} Assertion Failed: ${actual} !== ${expected}`);
+  return console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
 };
 
 assertEqual("Lighthouse", "Lighthouse");
