eac1f9313663b560d0bd78100786c9d26242244a
Anthony Chambers <ajfchambers@gmail.com>
2023-02-24 13:35:48 -0500

    created assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  const passed = String.fromCodePoint(0x2705, 0x2705, 0x2705);
+  const failed = String.fromCodePoint(0x274C, 0x274C, 0x274C);
+  if (eqArrays(array1, array2)) {
+    return console.log(`${passed} Assertion Passed: ${array1} === ${array2}`);
+  }
+  return console.log(`${failed} Assertion Failed: ${array1} !== ${array2}`);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
