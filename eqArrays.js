9d7991e5b0d2acc765ed02e1d3ca3c2bb427e3f9
Anthony Chambers <ajfchambers@gmail.com>
2023-02-24 13:13:48 -0500

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  //console.assert(actual === expected);
+  const passed = String.fromCodePoint(0x2705, 0x2705, 0x2705);
+  const failed = String.fromCodePoint(0x274C, 0x274C, 0x274C);
+  if (actual === expected) {
+    return console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
+  }
+  return console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Test Cases
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
