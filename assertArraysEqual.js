c5ed22c5cfe94ed6145be56dd7995dce97a9b04e
Anthony Chambers <ajfchambers@gmail.com>
2023-02-24 22:01:23 -0500

    updated test cases for asserArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -17,7 +17,7 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 // Test Cases
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 3], [1, 2, 3]);
-assertArraysEqual([1, 4, 3], [1, 2, 3]);
-assertArraysEqual([1, 5, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // Pass
+assertArraysEqual([1, 3], [1, 2, 3]); // Fail
+assertArraysEqual([1, 4, 3], [1, 2, 3]); // Fail
+assertArraysEqual([1, 5, 3], [1, 2, 3]); // Fail
