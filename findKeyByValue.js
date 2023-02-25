3b5b6619b54931a9ba3e8e4329b9d2aa0c2aee6d
Anthony Chambers <ajfchambers@gmail.com>
2023-02-25 18:16:48 +0000

    cleaned miss type findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -25,4 +25,4 @@ const bestTVShowsByGenre = {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "scifi");
