92318d1a3c2a4c5d78a236891ec7ff322d2aa860
Anthony Chambers <ajfchambers@gmail.com>
2023-02-25 14:25:53 +0000

    recursion code added, commented out

--- flatten.js
+++ flatten.js
@@ -16,6 +16,27 @@ const assertArraysEqual = function(actual, expected) {
   return console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
 };
 
+// Recursion Version
+/*
+const flatten = function(array) {
+  let flatArray = [];
+  for (const x in array) {
+    if (Array.isArray(array[x])) {
+      for (const y in array[x]) {
+        flatArray.push(array[x][y]);
+        if (Array.isArray(array[x][y])) {
+          flatArray = flatten(flatArray);
+        }
+      }
+      continue;
+    }
+    flatArray.push(array[x]);
+  }
+  return flatArray;
+};
+*/
+
+// No Recursion
 const flatten = function(array) {
   const flatArray = [];
   for (const x in array) {
@@ -34,4 +55,9 @@ const flatten = function(array) {
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6], [7, 8], 9]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 assertArraysEqual(flatten([1, 2, [3, 4], [5, 6], 7, 8, 9]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// Recursion Test Cases
+// console.log(flatten([1, 2, [3, 4], [5, [6, 7, [8, 9]]]]));
+// console.log(flatten([[1,[2, [3, 4]], [5, [6, 7, [8, 9]]]]]));
+// assertArraysEqual(flatten([1, 2, [3, 4], [5, [6, 7, [8, 9]]]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
