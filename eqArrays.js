f82bc52baae6cfd14a0a4196f07bc90c871e92b5
Anthony Chambers <ajfchambers@gmail.com>
2023-02-26 21:49:12 -0500

    assertions cleaned

--- eqArrays.js
+++ eqArrays.js
@@ -29,3 +29,4 @@ assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([], [1, 2, 3]), false);
+
