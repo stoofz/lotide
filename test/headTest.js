da57cc3a28c3ad6d05525d60e3c737fef3d31a73
Anthony Chambers <ajfchambers@gmail.com>
2023-03-06 20:49:16 -0500

    tail.js refactored and tailTest.js created

--- test/headTest.js
+++ test/headTest.js
@@ -4,5 +4,5 @@ const head = require('../head');
 // Test Cases
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello"); // Fail
+assertEqual(head([],''));
 assertEqual(head(["Hello"]), "Hello");
