33ddf64df7bdd9febf9f80d4a48adb4e6e00a7a8
Anthony Chambers <ajfchambers@gmail.com>
2023-03-08 11:36:06 -0500

    Tests refactored

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,10 +1,26 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const { eqArrays } = require('../index');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([], [1, 2, 3]), false);
+describe("#eqArrays", () => {
 
+  it("returns true, [1, 2, 3] equals [1, 2, 3] ", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("returns false, [1, 2, 3] does not equal [3, 2, 1] ", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+
+  it('returns true, ["1", "2", "3"] equals ["1", "2", "3"]', () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+
+  it('returns false, ["1", "2", "3"] does not equal ["1", "2", 3]', () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+
+  it("returns false, [] does not equal [1, 2, 3]", () => {
+    assert.deepEqual(eqArrays([], [1, 2, 3]), false);
+  });
+
+});
