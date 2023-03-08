e20bc35a04bdb28de3201fb3b6927ed09772596b
Anthony Chambers <ajfchambers@gmail.com>
2023-03-08 12:15:48 -0500

    tailTest.js descriptions modified

--- test/tailTest.js
+++ test/tailTest.js
@@ -7,23 +7,23 @@ const resultThree = tail([]);
 
 describe("#tail", () => {
 
-  it("returns 2 for result.length", () => {
+  it('returns 2 for result.length of ["Hello", "Lighthouse", "Labs"]', () => {
     assert.deepEqual(resultOne.length, 2);
   });
 
-  it("returns Lighthouse as first elemet", () => {
+  it('returns Lighthouse as first elemet of ["Hello", "Lighthouse", "Labs"]', () => {
     assert.deepEqual(resultOne[0], "Lighthouse");
   });
 
-  it("returns Labs as second elemet", () => {
+  it('returns Labs as second elemet ["Hello", "Lighthouse", "Labs"]', () => {
     assert.deepEqual(resultOne[1], "Labs");
   });
 
-  it("returns an empty array for its tail from an array with one element", () => {
+  it('returns an empty array for its tail from an array with one element, ["Hello"]', () => {
     assert.strictEqual(resultTwo.length, 0);
   });
 
-  it("returns an empty array for its tail from an empty array", () => {
+  it("returns an empty array for its tail from an empty array, []", () => {
     assert.strictEqual(resultThree.length, 0);
   });
 
