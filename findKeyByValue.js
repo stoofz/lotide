a552333a4835f1421fe08b363033f7e1bf696faf
Anthony Chambers <ajfchambers@gmail.com>
2023-03-09 13:03:33 -0500

    findKeyByValue.js refactored and test created

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 const findKeyByValue = function(object, showName) {
   for (const showType of Object.keys(object)) {
     if (showName === object[showType]) {
@@ -8,14 +6,4 @@ const findKeyByValue = function(object, showName) {
   }
 };
 
-const bestTVShowsByGenre = {
-  scifi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-// Test Cases
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "scifi");
+module.exports = findKeyByValue;
---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,30 @@
+const assert = require('chai').assert;
+const { findKeyByValue } = require("../index");
+
+const bestTVShowsByGenre = {
+  scifi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+describe("#findKeyByValue", () => {
+  
+  it("returns the key drama for the value of 'The Wire'", () => {
+    assert.deepEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+  
+  it("returns undefined for the value of 'That 70's Show' (non existant value)", () => {
+    assert.deepEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  })
+
+  it("returns the key comedy for the value of 'Brooklyn Nine-Nine'", () => {
+    assert.deepEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+  });
+  
+  it("returns the key scifi for the value of 'The Expanse'", () => {
+    assert.deepEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "scifi");
+  })
+
+});
+
+
