b91ee817ff74af562c0361f160e1b14346bb45c6
Anthony Chambers <ajfchambers@gmail.com>
2023-03-09 12:43:33 -0500

    findKey.js refactored and test created

--- findKey.js
+++ findKey.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 const findKey = function(object, callback) {
   for (const name of Object.keys(object)) {
     if (callback(object[name]) === true) {
@@ -9,23 +7,4 @@ const findKey = function(object, callback) {
   return;
 };
 
-
-assertEqual(
-  findKey({
-    "Blue Hill": { stars: 1 },
-    "Akaleri": { stars: 3 },
-    "noma": { stars: 2 },
-    "elBulli": { stars: 3 },
-    "Ora": { stars: 2 },
-    "Akelarre": { stars: 3 }
-  }, x => x.stars === 2), "noma");
-
-assertEqual(
-  findKey({
-    "Blue Hill": { stars: 1 },
-    "Akaleri": { stars: 3 },
-    "noma": { stars: 2 },
-    "elBulli": { stars: 3 },
-    "Ora": { stars: 2 },
-    "Akelarre": { stars: 3 }
-  }, x => x.stars === 3), "Akaleri");
+module.exports = findKey;
---
+++ test/findKeyTest.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert;
+const { findKey } = require("../index");
+
+describe("#findKey", () => {
+  
+  it("returns the key noma from object, which has a value of 2", () => {
+    assert.deepEqual(findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri": { stars: 3 },
+      "noma": { stars: 2 },
+      "elBulli": { stars: 3 },
+      "Ora": { stars: 2 },
+      "Akelarre": { stars: 3 }
+    }, x => x.stars === 2), "noma");
+  });
+  
+  it("returns the key Akaleri from object, which has a value of 3", () => {
+    assert.deepEqual(findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri": { stars: 3 },
+      "noma": { stars: 2 },
+      "elBulli": { stars: 3 },
+      "Ora": { stars: 2 },
+      "Akelarre": { stars: 3 }
+    }, x => x.stars === 3), "Akaleri");
+  })
+
+});
