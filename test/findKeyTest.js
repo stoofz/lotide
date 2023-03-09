50ef1bdbdda161de5a29934f38538753328fe915
Anthony Chambers <ajfchambers@gmail.com>
2023-03-09 12:51:38 -0500

    findKeyTest.js added test case

--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -3,7 +3,7 @@ const { findKey } = require("../index");
 
 describe("#findKey", () => {
   
-  it("returns the key noma from object, which has a value of 2", () => {
+  it("returns the key noma from object, which is the first key to have a value of 2", () => {
     assert.deepEqual(findKey({
       "Blue Hill": { stars: 1 },
       "Akaleri": { stars: 3 },
@@ -14,7 +14,7 @@ describe("#findKey", () => {
     }, x => x.stars === 2), "noma");
   });
   
-  it("returns the key Akaleri from object, which has a value of 3", () => {
+  it("returns the key Akaleri from object, which is the first key to have a value of 3", () => {
     assert.deepEqual(findKey({
       "Blue Hill": { stars: 1 },
       "Akaleri": { stars: 3 },
@@ -23,6 +23,17 @@ describe("#findKey", () => {
       "Ora": { stars: 2 },
       "Akelarre": { stars: 3 }
     }, x => x.stars === 3), "Akaleri");
+  })
+
+  it("returns the key undefined from object when value can't be found", () => {
+    assert.deepEqual(findKey({
+      "Blue Hill": { stars: 1 },
+      "Akaleri": { stars: 3 },
+      "noma": { stars: 2 },
+      "elBulli": { stars: 3 },
+      "Ora": { stars: 2 },
+      "Akelarre": { stars: 3 }
+    }, x => x.stars === 5), undefined);
   })
 
 });
