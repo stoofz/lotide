774d1b0d409c76ab042176307a175af8c3197cc8
Anthony Chambers <ajfchambers@gmail.com>
2023-03-10 15:41:22 -0500

    eqObjects refactored to support objects within objects, test updated

--- eqObjects.js
+++ eqObjects.js
@@ -11,6 +11,12 @@ const eqObjects = function(object1, object2) {
 
   for (const prop1 of Object.keys(object1)) {
 
+    if (!Array.isArray(object1[prop1]) && typeof object1[prop1] === 'object') {
+      if (eqObjects(object1[prop1], object2[prop1])) {
+        continue;
+      };
+    }
+
     if (Array.isArray(object1[prop1])) {
       if (eqArrays(object1[prop1], object2[prop1]) !== false) {
         continue;
--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -14,19 +14,39 @@ const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"
 describe("#eqObjects", () => {
   
   it("returns true, if both shirtObject's both have sizes medium and color red", () => {
-    assert.strictEqual(eqObjects(shirtObject , anotherShirtObject), true);
+    assert.deepEqual(eqObjects(shirtObject , anotherShirtObject), true);
   });
 
   it("returns false, longsleeveShirtObject has an extra key value of sleeveLength not in shirtObject", () => {
-    assert.strictEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+    assert.deepEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
   });
 
   it("returns true, if both multiColorShirtObject's have colors red, blue and are size medium", () => {
-    assert.strictEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+    assert.deepEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
   });
 
-  it("returns false, longSleeveMultiColorShirtObjects has additional sleeveLength parameter not in multiColorShirtObject ", () => {
-    assert.strictEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false);
+  it("returns false, longSleeveMultiColorShirtObjects has additional sleeveLength parameter not in multiColorShirtObject", () => {
+    assert.deepEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false);
   });
 
+  it("returns true, { a: { z: 1 }, b: 2 } matches { a: { z: 1 }, b: 2 } ", () => {
+    assert.deepEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+  });
+
+  it("returns false,  { a: { y: 0, z: 1 }, b: 2 } does not match { a: { z: 1 }, b: 2 }", () => {
+    assert.deepEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+  });
+
+  it("returns false, { a: { y: 0, z: 1 }, b: 2 } does not match { a: 1, b: 2 }", () => {
+    assert.deepEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+  });
+
+  it("returns true, { a: { z: 1, y: 2 }, b: 2 } matches { a: { z: 1, y: 2 }, b: 2 }", () => {
+    assert.deepEqual(eqObjects({ a: { z: 1, y: 2 }, b: 2 }, { a: { z: 1, y: 2 }, b: 2 }), true);
+  });
+
+  it("returns true, { a: { z: 1, y: { c: 2 } }, b: 2 } matches { a: { z: 1, y: { c: 2 } }, b: 2 }", () => {
+    assert.deepEqual(eqObjects({ a: { z: 1, y: { c: 2 } }, b: 2 }, { a: { z: 1, y: { c: 2 } }, b: 2 }), true);
+  });
+  
 });
