baf10cb0103df8db4ca04a8e14330ff52e4b2725
Anthony Chambers <ajfchambers@gmail.com>
2023-03-09 20:38:54 -0500

    flatten.js refactored and test created

--- flatten.js
+++ flatten.js
@@ -1,7 +1,3 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
-// Recursion Version
-/*
 const flatten = function(array) {
   let flatArray = [];
   for (const x in array) {
@@ -18,30 +14,5 @@ const flatten = function(array) {
   }
   return flatArray;
 };
-*/
 
-// No Recursion
-const flatten = function(array) {
-  const flatArray = [];
-  for (const x in array) {
-    if (Array.isArray(array[x])) {
-      for (const y in array[x]) {
-        flatArray.push(array[x][y]);
-      }
-      continue;
-    }
-    flatArray.push(array[x]);
-  }
-  return flatArray;
-};
-
-// Test Cases
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6], [7, 8], 9]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
-assertArraysEqual(flatten([1, 2, [3, 4], [5, 6], 7, 8, 9]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
-// Recursion Test Cases
-// console.log(flatten([1, 2, [3, 4], [5, [6, 7, [8, 9]]]]));
-// console.log(flatten([[1,[2, [3, 4]], [5, [6, 7, [8, 9]]]]]));
-// assertArraysEqual(flatten([1, 2, [3, 4], [5, [6, 7, [8, 9]]]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
+module.exports = flatten;
